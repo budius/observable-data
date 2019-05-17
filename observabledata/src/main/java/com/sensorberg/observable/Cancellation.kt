a44563327edf3d6c0fe63baa70448c6337d901aa
Ronaldo Pace <ronaldo.pace@sensorberg.com>
2019-05-17 16:45:25 +0200

    allow remove listener from cancellation

--- observabledata/src/main/java/com/sensorberg/observable/Cancellation.kt
+++ observabledata/src/main/java/com/sensorberg/observable/Cancellation.kt
@@ -2,26 +2,35 @@ package com.sensorberg.observable
 
 class Cancellation {
 
+	private val wrappers = mutableMapOf<(() -> Unit), CallbackWrapper>()
 	private val data = MutableObservableData<Boolean>()
 
 	val isCancelled: Boolean
 		get() = data.value == true
 
-	internal val source: ObservableData<Boolean>
-		get() = data
-
 	fun onCancelled(callback: () -> Unit) {
-		data.observe(object : Observer<Boolean> {
-			override fun invoke(t: Boolean?) {
-				if (t == true) {
-					data.removeObserver(this)
-					callback.invoke()
-				}
-			}
-		})
+		val wrapper = CallbackWrapper(callback)
+		wrappers[callback] = wrapper
+		data.observe(wrapper.observer)
+	}
+
+	fun removeCallback(callback: () -> Unit) {
+		val wrapper = wrappers.remove(callback)
+		wrapper?.let { data.removeObserver(it.observer) }
 	}
 
 	fun cancel() {
 		data.value = true
+	}
+
+	private inner class CallbackWrapper(val callback: () -> Unit) {
+		val observer = object : Observer<Boolean> {
+			override fun invoke(t: Boolean?) {
+				if (t == true) {
+					this@Cancellation.data.removeObserver(this)
+					callback.invoke()
+				}
+			}
+		}
 	}
 }
---
+++ observabledata/src/test/java/com/sensorberg/observable/CancellationTest.kt
@@ -0,0 +1,30 @@
+package com.sensorberg.observable
+
+import org.junit.Test
+
+import org.junit.Assert.*
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
+
+class CancellationTest {
+
+	@Test
+	fun receives_cancel_signal() {
+		val cancellation = Cancellation()
+		val wait = CountDownLatch(1)
+		cancellation.onCancelled { wait.countDown() }
+		cancellation.cancel()
+		assertTrue(wait.await(ObservableDataTest.TIMEOUT, TimeUnit.MILLISECONDS))
+	}
+
+	@Test
+	fun removes_cancel_listener() {
+		val cancellation = Cancellation()
+		val wait = CountDownLatch(1)
+		val listener = { wait.countDown() }
+		cancellation.onCancelled(listener)
+		cancellation.removeCallback(listener)
+		cancellation.cancel()
+		assertFalse(wait.await(ObservableDataTest.TIMEOUT, TimeUnit.MILLISECONDS))
+	}
+}
